b11227f7d25faca54513be6c77d5a4db32fd1fbb
nikhil16kulkarni <nikhil16kulkarni@gmail.com>
2023-03-21 23:14:10 -0400

    Part 2

---
+++ src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package edu.iu.p565.customerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.iu.p565.customerservice.model.Customer;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @GetMapping
+
+    public List<Customer> findAll(){
+        return null;
+    }
+
+    
+}
